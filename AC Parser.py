1c9a38c942df010711020a1955f220c59938cfae
Steven <52871164+sstonaker@users.noreply.github.com>
2021-10-17 15:17:09 -0500

    Increased accuracy of results.

--- AC Parser.py
+++ AC Parser.py
@@ -1,9 +1,9 @@
 import pdfplumber
 import re
 
-with pdfplumber.open(r'AC List3.pdf') as pdf:
+with pdfplumber.open(r'AC List2.pdf') as pdf:
     content = []
-    regs = {}
+    regs = {"pages": [], "variances": [], "regs": []}
     pages = pdf.pages
     terms = ['departure', 'alternate', 'alternate compliance',
              'AC', 'A/C', 'variance' 'alt compliance']
@@ -11,16 +11,28 @@ with pdfplumber.open(r'AC List3.pdf') as pdf:
         content = page.extract_text()
         hits = 0
         for term in terms:
-            if term in content:
-                hits += 1
+            for word in content.split(' '):
+                if term in word:
+                    hits += 1
 
         reg = re.findall('250\.(.+?)\s', content)
-        regs.update({page: reg})
-        if hits > 0:
-            print(f"Found {hits} variance(s) on {page}.")
+        regs["pages"].append(page)
+        regs["variances"].append(hits)
+        regs["regs"].append(reg)
+
+        print(
+            f"Found {hits} variance(s) and {len(reg)} regulation(s) on {page}.")
+        if reg:
             print(f'\t {reg}\n')
         else:
-            print(f'{hits} variance(s) on {page}\n')
+            print(f'\t')
+
     print("Summary:")
-    for k, v in regs.items():
-        print(k, v)
+    print(f"Pages = {len(regs['pages'])}")
+    print(f"Variances = {sum(regs['variances'])}")
+    regs_set = []
+    for v in regs["regs"]:
+        for inner_v in v:
+            if inner_v not in regs_set:
+                regs_set.append(inner_v)
+    print(f"Regulations = {set(regs_set)}")
